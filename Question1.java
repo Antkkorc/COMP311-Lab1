f1c9fb8e009b5bcbf16b11e80a99c19e6aa62196
Antonio Koketso <antoniokealeboga1@gmail.com>
2026-08-30 23:01:14 +0200

    Add Question 1: personal profile with BIUST details

---
+++ Question1.java
@@ -0,0 +1,16 @@
+public class Question1 {
+    public static void main(String[] args) {
+        //my personal profile
+        String name = "Antonio Koketso";
+        int age = 21;
+        double gpa = 4.9;
+        boolean fullTime = true;
+
+        //printing the profile
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Full-time: " + fullTime);
+    }
+
+}
